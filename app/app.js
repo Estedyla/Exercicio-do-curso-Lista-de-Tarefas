460989aeb318bc58eea0e4ebfc9a5132c2786dc7
Estédyla Hansen <95441721+Estedyla@users.noreply.github.com>
2022-05-16 14:46:44 -0300

    Add files via upload

---
+++ app/app.js
@@ -0,0 +1,37 @@
+console.log ("Olá Mundo");
+
+var tarefa1 = "Ir ao supermercado"
+var tarefa2 = "Estudar Programação"
+var tarefa3 = "Atualizar o Linkedin"
+
+console.log("Minhas tarefas são:", tarefa1,  tarefa2, tarefa3)
+
+const tarefas= [];
+
+tarefas.push(tarefa1)
+tarefas.push(tarefa2)
+tarefas.push(tarefa3)
+
+console.log(tarefas);
+
+var novaTarefa = 'Regar plantas'
+
+alert("Sua nova tarefa é: " + novaTarefa);
+
+tarefas.push(novaTarefa);
+
+console.log(tarefas);
+
+//loops
+
+for (i=0; i<tarefas.length; i++) 
+
+console.log("Minha tarefa é " + tarefas[i]);
+
+//Funções
+let= "tarefas"
+function exibirTarefas(tarefas) {
+    i=0; i<tarefas.length; i++; 
+    
+}
+console.log(exibirTarefas);
